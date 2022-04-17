9b138096e399b10d8101ebcb132e394f7b162ebc
Luis Mota <38186@ufp.edu.pt>
2022-04-17 19:11:49 +0100

    Register position

--- src/components/Position.svelte
+++ src/components/Position.svelte
@@ -1,8 +1,54 @@
 <script>
-    let message = 'Position';
+    import axios from "axios";
+    import { createEventDispatcher } from "svelte";
+    import { userID } from "../stores/store";
+    import { position } from "../Routes.svelte";
 
+    const dispatch = createEventDispatcher();
+    let message = { success: null, display: "" };
+    let isSubmitting = false
+    let coordinates = []
+    let long = 0
+    let lat = 0
+
+    $: submit = async () => {
+            document.getElementById("submit").disabled = true;
+            isSubmitting = true;
+            // giving error message "Check syntax!"
+            const response = await axios.post(position.registerLocation, {
+                    //userid: $userID,
+                    UserID: $userID,
+                    Latitude: lat,
+                    Longitude: long
+            });
+
+            if (response.status === 201) {
+                    message = { success: true, display: response.data.message };
+                    new Promise((resolve) => setTimeout(resolve, 500)).then(() => {
+                            isSubmitting = false;
+                            //dispatch("position_added", {followers: response.data.followers})
+                            document.getElementById("submit").disabled = false;
+                    });
+            } else {
+                    message = { success: false, display: response.data.message };
+                    isSubmitting = false;
+                    document.getElementById("submit").disabled = false;
+            }
+    }
 </script>
 
-<div class="container mt-5 text-center">
-    <h3>{message}</h3>
-</div>
+<form on:submit|preventDefault={submit}>
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label>New Coordinates</label>
+    <input name="latitude" placeholder="Set Follower ID" bind:value={lat} />
+    <input name="longitude" placeholder="Set Follower ID" bind:value={long} />
+    <button type="submit" id="submit">
+            {#if isSubmitting}Adding...{:else}Add{/if}
+    </button>
+</form>
+
+{#if message.success != null}
+    <div class="alert {message.success ? 'alert-success' : 'alert-danger'}" role="alert">
+            {message.display}
+    </div>
+{/if}
