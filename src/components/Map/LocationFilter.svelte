abd23038e5de98265bce457a8fb51351c41146cf
LuisMota1999 <38186@ufp.edu.pt>
2022-04-20 14:32:06 +0100

    Checkbox fix map

--- src/components/Map/LocationFilter.svelte
+++ src/components/Map/LocationFilter.svelte
@@ -98,12 +98,12 @@
 		<button class="submit" id="location_history" on:click={location_history}>Location History</button>
 		
 	</div>
-	 
+	
 	<div clas="row" style="color:black;display:flex; justfiy-content:center; margin-top:10%">
 		
-			<label for="searchID" style="text-align:center;margin-left:1%; margin-top:3%">Search By ID</label>
-			<div class="col-sm-6">
-				<input id="idFilter" class="form-content" style="margin-top:3%"  type="number" bind:value={followerID} autocomplete="on" />
+			<div class="col-sm-6" style="margin-left:2%;">
+				<label style="color:black;">Follower ID</label>
+				<input name="idFilter" style="width:75%; margin-top:3%;display:flex; justify-content:center;align-items:center;"  placeholder="Set Follower ID"   bind:value={followerID} autocomplete="on" />
 			</div>
 			<button class="submit" id="follower_locations"  style="margin-top:0%;" on:click={follower_locations}>Follower Location</button>
 
@@ -111,17 +111,25 @@
 
 
 	</div>
-	<div class="row" style="margin-top:2%">
-		<input type="checkbox" id="filterDate" name="filterDate" bind:checked>
-		<div class="col-sm-6">
-			<label for="sdFilter">Start Date</label>
-			<input type="date" id="sdFilter" name="filterDate" bind:value={startDate} placeholder="Search by Start Date" disabled={!checked}>
+
+	<div class="col-sm-12">
+		<div class="row" style="margin-top:10%">
+			<div class="col-sm-5">
+			
+			<input type="date" id="sdFilter" style="width:100%;background-color:white;color:black; margin-top:3%;display:flex; justify-content:center;align-items:center;" name="filterDate" bind:value={startDate} placeholder="Search by Start Date" disabled={!checked}>
+			</div>
+
+			<div class="col-sm-5">
+			
+			<input type="date"  style="width:100%; background-color:white; color:black;margin-top:3%;display:flex; justify-content:center;align-items:center;" id="edFilter" name="filterDate" bind:value={endDate} placeholder="Search by End Date" disabled={!checked}>
+			</div>
+			<div class="col-sm-2">
+				<input type="checkbox" id="filterDate" name="filterDate" bind:checked>~
+			</div>
 		</div>
-		<div class="col-sm-6">
-			<label for="edFilter">End Date</label>
-			<input type="date" id="edFilter" name="filterDate" bind:value={endDate} placeholder="Search by End Date" disabled={!checked}>
-		</div>
-	</div>
+</div>
+
+
 	<div class="col-sm-12">
 	<div class="row" style="margin-top:2%;height:200px; overflow-y: scroll">
 		
@@ -276,6 +284,130 @@
 	h4 {
 	  margin: 2rem 0rem 1rem;
 	}
-	
+	input[type=checkbox] {
+                    --active: #ed9121;
+                    --active-inner: #fff;
+                    --focus: 2px rgba(39, 94, 254, .3);
+                    --border: #797979;
+                    --border-hover: #ed9121;
+                    --background: #fff;
+                    --disabled: #F6F8FF;
+                    --disabled-inner: #E1E6F9;
+                    -webkit-appearance: none;
+                    -moz-appearance: none;
+                    height: 21px;
+                    outline: none;
+                    display: inline-block;
+                    vertical-align: top;
+                    position: relative;
+                    margin: 0;
+                    cursor: pointer;
+                    border: 1px solid var(--bc, var(--border));
+                    background: var(--b, var(--background));
+                    transition: background 0.3s, border-color 0.3s, box-shadow 0.2s;
+                }
+
+                    input[type=checkbox]:not(.switch) {
+                        --active: #ed9121;
+                        --active-inner: #fff;
+                        --focus: 2px rgba(39, 94, 254, .3);
+                        --border: #797979;
+                        --border-hover: #ed9121;
+                        --background: #fff;
+                        --disabled: #F6F8FF;
+                        --disabled-inner: #E1E6F9;
+                        -webkit-appearance: none;
+                        -moz-appearance: none;
+                        height: 25px;
+                        outline: none;
+                        display: inline-block;
+                        vertical-align: top;
+                        position: relative;
+                        margin: 0;
+                        cursor: pointer;
+                        border: 1px solid var(--bc, var(--border));
+                        background: var(--b, var(--background));
+                        transition: background 0.3s, border-color 0.3s, box-shadow 0.2s;
+                    }
+
+                    input[type=checkbox]:after {
+                        content: "";
+                        display: block;
+                        left: 0;
+                        top: 0;
+                        position: absolute;
+                        transition: transform var(--d-t, 0.3s) var(--d-t-e, ease), opacity var(--d-o, 0.2s);
+                    }
+
+                    input[type=checkbox]:checked {
+                        --b: var(--active);
+                        --bc: var(--active);
+                        --d-o: .3s;
+                        --d-t: .6s;
+                        --d-t-e: cubic-bezier(.2, .85, .32, 1.2);
+                    }
+
+                    input[type=checkbox]:disabled {
+                        --b: var(--disabled);
+                        cursor: not-allowed;
+                        opacity: 0.9;
+                    }
+
+                        input[type=checkbox]:disabled:checked {
+                            --b: var(--disabled-inner);
+                            --bc: var(--border);
+                        }
+
+                        input[type=checkbox]:disabled + label {
+                            cursor: not-allowed;
+                        }
+
+                    input[type=checkbox]:hover:not(:checked):not(:disabled) {
+                        --bc: var(--border-hover);
+                    }
+
+                    input[type=checkbox]:focus {
+                        box-shadow: 0 0 0 var(--focus);
+                    }
+
+                    input[type=checkbox]:not(.switch) {
+                        width: 25px;
+                    }
+
+                        input[type=checkbox]:not(.switch):after {
+                            opacity: var(--o, 0);
+                        }
+
+                        input[type=checkbox]:not(.switch):checked {
+                            --o: 1;
+                        }
+
+                    input[type=checkbox] + label {
+                        font-size: 14px;
+                        line-height: 21px;
+                        display: inline-block;
+                        vertical-align: top;
+                        cursor: pointer;
+                        margin-left: 4px;
+                    }
+
+                    input[type=checkbox]:not(.switch) {
+                        border-radius: 7px;
+                    }
+
+                        input[type=checkbox]:not(.switch):after {
+                            width: 9px;
+                            height: 15px;
+                            border: 2px solid var(--active-inner);
+                            border-top: 0;
+                            border-left: 0;
+                            left: 7px;
+                            top: 2px;
+                            transform: rotate(var(--r, 20deg));
+                        }
+
+                        input[type=checkbox]:not(.switch):checked {
+                            --r: 43deg;
+                        }
 	
 </style>
