32f8ecd050937b7b09781c50e95fafb582d32d5d
Luis Mota <38186@ufp.edu.pt>
2022-04-21 01:50:06 +0100

    Alert in XKms

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -7,7 +7,7 @@
     import Follower  from './components/Followers/Follower.svelte';
     import SOS       from './components/SOS/SOS.svelte';
     import AlertTime from './components/Alert/AlertTime.svelte';
-
+    import AlertNearBy from './components/AlertNearBy/TableNearBy.svelte'
 
     // these are our 'pages' to navigate through (in order for this export to work the script tag MUST include context="module")
 	export const navOptions = [
@@ -15,8 +15,9 @@
 		{ page: 'Login',      component: Login,     loggedIn: "false" },
         { page: 'Register',   component: Register,  loggedIn: "false" },
         { page: 'Location',   component: Location,  loggedIn: "true" },
+        { page: 'Followers NearBy', component: AlertNearBy, loggedIn: "true"},
         { page: 'Follower',   component: Follower,  loggedIn: "true" },
         { page: 'SOS',        component: SOS,       loggedIn: "true" },
-        { page: 'Alert Time', component: AlertTime, loggedIn: "true" },
+        { page: 'Alert Time', component: AlertTime, loggedIn: "true" }
 	];
 </script>
